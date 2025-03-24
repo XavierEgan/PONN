5e0eabc7a26ee791ca37f0dcf0890f3ef07a183f
XavierEgan <xavieregan2@gmail.com>
2025-03-24 14:51:28 +1100

    removed java stuff in preperation for python rewrite

--- WIP_DONT_USE/GeneticTrainer/TrainerFunctions/Environment.ts
+++
@@ -1,8 +0,0 @@
-import { Network } from "../../../Neural_Network/Network.ts";
-import { GeneticAlgorithm } from "../../../GeneticAlgorithm/GeneticAlgorithm.ts";
-
-export class Environment {
-    constructor() {
-        //
-    }
-}
--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-import { Network } from "../Neural_Network/Network.ts";
-import { ActivationFunction } from "../Neural_Network/Activation_Functions.ts";
-
-let test_network = new Network();
-
-test_network.load_network_from_file("test.json")
-
-console.log(test_network.forward([[789534]])[0]);
